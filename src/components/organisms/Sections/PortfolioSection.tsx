b0e93ef2ac67ed4387595a888749c68a4148efbf
hisashin <dawid.szemborowski@aexol.com>
2022-09-05 23:26:23 +0200

    fix errors

--- src/components/organisms/Sections/PortfolioSection.tsx
+++ src/components/organisms/Sections/PortfolioSection.tsx
@@ -1,10 +1,5 @@
 import React from 'react'
 
 export const PortfolioSection: React.FC = () => {
-    return (
-        <div className="flex items-center justify-between">
-            {/* <SectionTitle title="Portfolio" subtitle="Projects I made" moreUrl="/portfolio" /> */}
-            {/* <SectionImage sectionId="portfolio-section" /> */}
-        </div>
-    )
+    return <div className="flex items-center justify-between"></div>
 }
