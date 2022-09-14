a3f129cce319ac3418551faa211aaf57ba2ac96a
hisashin <dawid.szemborowski@aexol.com>
2022-09-15 01:13:48 +0200

    init gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { Layout } from 'layouts'
 import { NextPage } from 'next'
 import { AnimatePresence } from 'framer-motion'
+import TagManager from 'react-gtm-module'
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
+    useEffect(() => {
+        TagManager.initialize({
+            gtmId: process.env.NEXT_PUBLIC_GTM_ID || 'G-RCRRMRVD7C'
+        })
+    }, [])
     return (
         <AnimatePresence>
             <Layout>
