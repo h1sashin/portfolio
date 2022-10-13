d4da01564dbad98b96033ed3afdf29479343b00e
hisashin <dawid.szemborowski@aexol.com>
2022-10-13 19:16:51 +0200

    manifest.json

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,8 @@ const Document: DocumentType = () => {
         <Html lang="en" className="bg-black">
             <Head>
                 <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+                <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
+                <meta name="theme-color" content="#353e48" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap" rel="stylesheet" />
