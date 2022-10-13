7d2c8261724136b833262952a289e582347d5faa
hisashin <dawid.szemborowski@aexol.com>
2022-10-13 18:10:18 +0200

    add alt to images

--- src/components/atoms/Skill/Skill.tsx
+++ src/components/atoms/Skill/Skill.tsx
@@ -3,7 +3,7 @@ import { SkillProps } from './types'
 
 export const Skill: React.FC<SkillProps> = ({ name, icon }) => (
     <div className="flex flex-col gap-4 justify-center items-center w-max h-max">
-        <img className="h-24 aspect-square" src={icon?.url} />
+        <img alt={`${name} logo`} className="h-24 aspect-square" src={icon?.url} />
         <span className="text-lg text-white font-semibold">{name}</span>
     </div>
 )
--- src/components/organisms/Sections/HeroSection.tsx
+++ src/components/organisms/Sections/HeroSection.tsx
@@ -8,12 +8,12 @@ export const HeroSection: React.FC<HeroProps> = ({ company, links }) => {
     return (
         <section id="hero" className="min-h-screen w-full flex relative items-center justify-center">
             <div className="absolute right-16 top-3/4">
-                <Image src="https://svgsilh.com/svg/26432.svg" height="256" width="256" />
+                <Image alt="dotted square" src="https://svgsilh.com/svg/26432.svg" height="256" width="256" />
             </div>
             <div className="w-full flex flex-col relative h-max gap-6 sm:gap-16">
                 <div className="flex flex-col">
                     <div className="absolute -left-4  -top-6 -z-10">
-                        <Image src="https://svgsilh.com/svg/26432.svg" height="128" width="128" />
+                        <Image alt="dotted square" src="https://svgsilh.com/svg/26432.svg" height="128" width="128" />
                     </div>
                     <p className="text-2xl sm:text-3xl xl:text-5xl text-primary">Hey there!, I'm</p>
                     <h1 className="text-4xl sm:text-6xl xl:text-8xl font-bold text-white leading-normal">
